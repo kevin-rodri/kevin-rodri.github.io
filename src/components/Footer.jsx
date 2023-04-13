a3baca296ae6a436a95aae1a01a2f21c2249ed30
Kevin Rodriguez <97323085+kevin-rodri@users.noreply.github.com>
2023-04-13 16:29:51 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="copy">
-        <p>&copy; {new Date().getFullYear().toPrecision()} Kevin Rodriguez</p>
+        <p>&copy; 2023 Kevin Rodriguez</p>
       </div>
     </footer>
   );
