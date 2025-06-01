fc900ab9b6be8708b7c4dd56d31ca58b2c07bd14
kevin-rodri <kr399880@gmail.com>
2025-06-01 00:46:45 -0400

    personal portfolio changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,12 +9,8 @@ function About() {
         <h2 id="about-heading">About Me</h2>
 
       <p>
-       Hey there! I'm Kevin Rodriguez, and I am currently pursuing a bachelor's degree in software engineering with a minor in mathematics at <a href="https://www.qu.edu">Quinnipiac University</a>.
+       Hey there! I'm Kevin Rodriguez, and I graduated with a bachelor's degree in software engineering from <a href="https://www.qu.edu">Quinnipiac University</a>.
         My journey in the world of code is a thrilling exploration of creativity and problem-solving.
-      </p>
-      <p>
-      As a software engineering student, I am passionate about designing and building efficient software solutions to real-world problems. My minor in mathematics has given me a solid 
-      foundation in logical reasoning and critical thinking, essential software development skills. 
       </p>
       <p>
       In my free time, I enjoy working on personal coding projects, trying new caffeinated beverages, 
--- src/services/projectServices.jsx
+++ src/services/projectServices.jsx
@@ -3,20 +3,28 @@ import stockMarketImage from "../photos/StockMarketMastery.png";
 import hiredAIImage from "../photos/hiredaipicture.png";
 import openLabImage from "../photos/openlab.png";
 import aiConnectImage from "../photos/aiconnect.png";
+import ehrApplicationImage from "../photos/ehr-application.png";
 
 const projects = [
+  {
+    title: "EHR-Application",
+    image: ehrApplicationImage,
+    description: "For my senior capstone project, I co-developed an Electronic Health Record (EHR) application for Quinnipiac's School of Nursing. The applicatio does not only offer real-time responsive updates and instructorsand administrators are unable to provide provider orders and lab results in real-time during a simulation. In addition, the application enables students, instructors, and administrators within Quinnipiac University's School of Nursing to manage real-time patient information during simulation labs. The application focuses on creating an environment for everyone within the School of Nursing to access and simulate real-life healthcare scenarios successfully. Nursing students and instructors can input patient information based on their simulation. Instructors and administrators will be able to build and update the patient information.   ",
+    sourceCodeLink: "https://ehr-web-application.vercel.app/",
+    demoLink: "https://quinnipiac.zoom.us/rec/play/8fCJDOZVR4TvfMK9hkwmhkY6zVeMMrehuGQNQQy7AZF32GsjbnHQtlbnGnQ6GISmF03Bk8codnTsWjj2.zLzxj6324_tmeGIC?autoplay=true&startTime=1744406088000",
+  },
   {
     title: "AI Connect",
     image: aiConnectImage,
     description: "I co-developed an Android chat application using Kotlin that enables users to chat with an AI in 108 languages using speech and text inputs. The app integrates Amazon Polly and Google TTS for text-to-speech conversion, supporting 41 languages. The backend uses Amazon Web Services (AWS), with an AWS Lambda function as the interface between the frontend and Amazon Polly and an S3 bucket for storing generated audio files. Speech recognition is one of the many features added to support voice input in multiple languages. To reduce the need for repeated calls to the backend service, a LruCache for caching audio resulted in a 50% reduction in calls. Google's Gemini API generates conversations. I implemented Firebase for user authentication and MongoDB as our database backend to allow users to resume conversations.",
     sourceCodeLink: "https://github.com/Ajifolahan/AI_Dialogue_Assistant",
     demoLink: "https://momoreayinde.dev/Images/aiConnect.mp4"
-  }, 
+  },
   {
     title: "OpenLab Scheduling Application",
     image: openLabImage,
     description: "I developed an OpenLab Scheduling application tailored for Quinnipiac University's School of Nursing. This project spanned throughout the school year, during which I collaborated with peers and faculty members to create a user-friendly platform for managing lab sessions. Using agile methodologies and numerous scrum sprints, we designed and implemented features enabling students to register for open and make-up lab sessions. Professors can dynamically add or delete labs and track student attendance efficiently. My role involved close collaboration with stakeholders to gather requirements, develop and test features, and ensure the platform met the unique needs of nursing students and faculty.",
-    sourceCodeLink: "https://github.com/AaryanKapoor22/OpenLab", 
+    sourceCodeLink: "https://github.com/AaryanKapoor22/OpenLab",
     documentationLink: "https://open-lab-scheduling.vercel.app"
   },
   {
@@ -25,12 +33,6 @@ const projects = [
     description: "Developed a Chrome extension leveraging HTML, CSS, and JavaScript to dynamically generate tailored cover letters and resume by intelligently analyzing user experience against job description. Integrated multiple APIs for precise job data extraction, resume parsing, and utilized GPT-3.5 API to craft individualized cover letters, resulting in a 30% efficacy boost and an exceptional 100%-time savings. Collaborated with a team of three ensuring seamless task delegation and fostering intercommunication.",
     sourceCodeLink: "https://github.com/GandhiHarsh2003/CoverLetterGenerator",
     demoLink: "https://gandhiharsh2003.github.io/Images/hiredai.mp4"
-  },
-  {
-    title: "Stock-Market-Mastery",
-    image: stockMarketImage,
-    description: "An Android application that allows users to learn more about stocks. The user can search through thousands of stock companies and retain more information about a company's stock. If they're convinced, the user can purchase that company's stock or continue to look for other stocks. Once a stock is bought, the user can sell it in return for some profit based on how much they're willing to sell. This project was worked on alongside Harsh Gandhi, a Computer Science and Data Science undergraduate at Quinnipiac.",
-    sourceCodeLink: "https://github.com/GandhiHarsh2003/Stock-Market-Mastery"
   },
   {
     title: "Project Z",
